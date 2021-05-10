586958d767245747c3cbf1ee44d7034c19c597a6
Luca <eslam.m.mostafa@outlook.com>
2021-05-10 23:35:35 +0200

    solve publishing issue

--- index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import render_template
-from flask import url_for
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return render_template("/index.html")
